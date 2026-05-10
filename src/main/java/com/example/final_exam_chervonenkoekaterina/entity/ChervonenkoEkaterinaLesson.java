ca1b7990033d2c265fc5d9c052992631f29c56e5
ebashmak <katysik.chervonenko@gmail.com>
2026-05-10 22:26:37 +0500

    add lesson entity

--- src/main/java/com/example/final_exam_chervonenkoekaterina/entity/ChervonenkoEkaterinaLesson.java
+++ src/main/java/com/example/final_exam_chervonenkoekaterina/entity/ChervonenkoEkaterinaLesson.java
@@ -1,4 +1,23 @@
 package com.example.final_exam_chervonenkoekaterina.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Table(name = "lessons")
+@Data
 public class ChervonenkoEkaterinaLesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    private String videoUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private ChervonenkoEkaterinaCourse course;
 }
