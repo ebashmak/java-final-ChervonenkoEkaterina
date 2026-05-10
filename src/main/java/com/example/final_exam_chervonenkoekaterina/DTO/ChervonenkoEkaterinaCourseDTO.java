8d66171ca7361d5c3e625888c5e4d7a88ef28590
ebashmak <katysik.chervonenko@gmail.com>
2026-05-11 00:09:25 +0500

    валидация

--- src/main/java/com/example/final_exam_chervonenkoekaterina/DTO/ChervonenkoEkaterinaCourseDTO.java
+++ src/main/java/com/example/final_exam_chervonenkoekaterina/DTO/ChervonenkoEkaterinaCourseDTO.java
@@ -8,10 +8,12 @@ import lombok.Data;
 public class ChervonenkoEkaterinaCourseDTO {
     private Long id;
 
+    @NotBlank(message = "Название курса обязательно")
     private String title;
 
     private String description;
 
+    @Min(value = 0, message = "Цена не может быть отрицательной")
     private Double price;
 
     private Long categoryId;
