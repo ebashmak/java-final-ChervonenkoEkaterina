5e3e683b5be4bab84bb121e80ee7cbea8323c954
ebashmak <katysik.chervonenko@gmail.com>
2026-05-10 23:42:35 +0500

    feat: add main application class with Async support enabled

---
+++ src/main/java/com/example/final_exam_chervonenkoekaterina/FinalExamChervonenkoEkaterinaApplication.java
@@ -0,0 +1,15 @@
+package com.example.final_exam_chervonenkoekaterina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class FinalExamChervonenkoEkaterinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamChervonenkoEkaterinaApplication.class, args);
+    }
+
+}
