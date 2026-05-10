969abf6d7ac23909d0518ca3b16bf12216f33848
ebashmak <katysik.chervonenko@gmail.com>
2026-05-10 22:29:18 +0500

    add Enrollment entity

---
+++ src/main/java/com/example/final_exam_chervonenkoekaterina/entity/ChervonenkoEkaterinaEnrollment.java
@@ -0,0 +1,22 @@
+package com.example.final_exam_chervonenkoekaterina.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+@Entity
+@Table(name = "enrollments")
+@Data
+public class ChervonenkoEkaterinaEnrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private ChervonenkoEkaterinaUser student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private ChervonenkoEkaterinaCourse course;
+
+    private java.time.LocalDateTime enrollmentDate = java.time.LocalDateTime.now();
+}
