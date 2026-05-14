2df39a66f5303fcbdede29b321df30e6451714fe
ebashmak <katysik.chervonenko@gmail.com>
2026-05-14 22:40:35 +0500

    add CategoryDTO

--- src/main/java/com/example/final_exam_chervonenkoekaterina/DTO/ChervonenkoEkaterinaCategoryDTO.java
+++ src/main/java/com/example/final_exam_chervonenkoekaterina/DTO/ChervonenkoEkaterinaCategoryDTO.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChervonenkoEkaterinaCategoryDTO {
-
     private Long id;
 
     @NotBlank(message = "Название категории не может быть пустым")
